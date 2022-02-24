04455a32715c9c6fc4b91008a888ebe2d15d48f8
winston mhango <71964085+winstonmhango23@users.noreply.github.com>
2022-02-24 20:21:33 +0200

    altered langauges

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -33,8 +33,8 @@ const Header = () => (
         </div>
 
         <div className="tag-cmp app__flex">
-          <p className="p-text">Web Developer</p>
-          <p className="p-text">Freelancer</p>
+          <p className="p-text">A Python, Django, Flask</p>
+          <p className="p-text">React & Tailwind css Developer</p>
         </div>
       </div>
     </motion.div>
